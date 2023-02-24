2ce5860377404032eee3fa3bfaa57189fb294b60
Zhang Jing <mrzleo@foxmail.com>
2023-02-24 17:26:50 +0800

    simple http handler

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,18 @@
 #![deny(warnings)]
 
-use std::env;
-
 use hyper::{
     rt::{self, Future},
     service::service_fn_ok,
     Body, Response, Server,
 };
 extern crate hyper;
-extern crate pretty_env_logger;
 
 const DEFAULT_PORT: u16 = 8080;
 
 fn run_server(port: u16) {
-    pretty_env_logger::init();
-
     let addr = ([0, 0, 0, 0], port).into();
 
-    let service = move || {
-        service_fn_ok(move |_| {
-            let hello = [
-                format!("[{}]:", port),
-                "hello".to_owned(),
-                env::var("TARGET").unwrap_or_else(|_| "Rust demo!".to_owned()),
-            ]
-            .join(" ")
-                + "\n";
-            Response::new(Body::from(hello))
-        })
-    };
+    let service = move || service_fn_ok(move |_| Response::new(Body::from("spin-rs")));
 
     let server = Server::bind(&addr)
         .serve(service)
