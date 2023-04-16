13da06160490fc2b184c4d49397337839900320d
Zhang Jing <mrzleo@foxmail.com>
2023-04-16 16:36:01 +0800

    feat: Add support to boot a instance when first request come

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,18 @@ use hyper::{
     service::{make_service_fn, service_fn},
     Body, Request, Response, Server, StatusCode,
 };
-use std::net::{IpAddr, SocketAddr};
+use std::{
+    net::{IpAddr, SocketAddr},
+    sync::atomic::{AtomicBool, Ordering},
+};
+use tokio::process::Command;
 
-static INSTANCE_IP: &str = "http://0.0.0.0:8079";
+static INSTANCE_ADDR: &str = "http://0.0.0.0:8079";
 
 #[tokio::main]
 async fn main() {
-    let default_ip = "0.0.0.0:8080";
-    let addr: SocketAddr = default_ip.parse().unwrap();
+    let default_addr = "0.0.0.0:8080";
+    let addr: SocketAddr = default_addr.parse().unwrap();
 
     let make_svc = make_service_fn(|conn: &AddrStream| {
         let remote_addr = conn.remote_addr().ip();
@@ -26,8 +30,22 @@ async fn main() {
     }
 }
 
+static INSTANCE_BOOT: AtomicBool = AtomicBool::new(false);
+
+/// Send the request to the real instance
 async fn handle(client_ip: IpAddr, req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    match hyper_reverse_proxy::call(client_ip, INSTANCE_IP, req).await {
+    // check whether instance exsit
+    if !INSTANCE_BOOT.load(Ordering::Relaxed) {
+        tokio::spawn(async move {
+            Command::new("python3")
+                .arg("daemon.py")
+                .spawn()
+                .expect("fail to boot instance")
+        });
+        INSTANCE_BOOT.store(true.into(), Ordering::Relaxed);
+    }
+
+    match hyper_reverse_proxy::call(client_ip, INSTANCE_ADDR, req).await {
         Ok(response) => Ok(response),
         Err(_) => Ok(Response::builder()
             .status(StatusCode::INTERNAL_SERVER_ERROR)
