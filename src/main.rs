bdbab80d2fa1818e45cce81c1fa049203b4c550d
Zhang Jing <mrzleo@foxmail.com>
2023-04-19 13:47:13 +0800

    feat: Add support instance dynamic monitor, kill instance if don't get request for a long time

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,28 @@ use hyper::{
 };
 use std::{
     net::{IpAddr, SocketAddr},
-    time::{SystemTime, UNIX_EPOCH},
+    time::{Duration, SystemTime, UNIX_EPOCH},
 };
-use tokio::{process::Command, sync::OnceCell};
+use tokio::{process::Command, sync::OnceCell, time::sleep};
+
+static INSTANCE_ADDR: &str = "http://0.0.0.0:8079";
+
+/// # SARETY
+/// Only `kill_instance()` will do write operation, other will use protected
+/// operation inside the `OnceCell`.
+///
+/// Only `server.await` and `monitor()` will run parallelly, but only one task can
+/// return. `monitor()` use `initialized()` while `server.await` use
+/// `get_or_init()`. These two operation is protected.
+///
+/// Only if `monitor()` is finished, `kill_instance()` can be call. `server.await`
+/// can not continue if `monitor()` is finished. `kill_instance()` can access
+/// and take the ownership, so that next time call `get_or_init` will init a
+/// new OnceCell.
+///
+/// Only if `kill_instance()` is finished, next `select!` can run, i.e., next
+/// `get_or_init()` can be called.
+static mut INSTANCE: OnceCell<u32> = OnceCell::const_new();
 
 #[tokio::main]
 async fn main() {
@@ -23,53 +42,91 @@ async fn main() {
     });
 
     let server = Server::bind(&addr).serve(make_svc);
-    if let Err(e) = server.await {
-        eprintln!("server error :{e}");
+    tokio::pin!(server);
+    loop {
+        tokio::select! {
+            _ = &mut server => {
+                continue;
+            }
+            _ = monitor() => {
+                kill_instance().await;
+            }
+        }
     }
 }
 
-static INSTANCE_ADDR: &str = "http://0.0.0.0:8079";
-static INSTANCE_START: OnceCell<()> = OnceCell::const_new();
+async fn monitor() {
+    unsafe {
+        while !INSTANCE.initialized() {
+            sleep(Duration::from_secs(1)).await;
+        }
+        // find instance initialized
+        // start to sleep until
+        if INSTANCE.initialized() {
+            sleep(Duration::from_secs(10)).await;
+        }
+    }
+}
+
+async fn kill_instance() {
+    unsafe {
+        Command::new("kill")
+            .arg("-9")
+            .arg(INSTANCE.take().unwrap().to_string().as_str())
+            .spawn()
+            .unwrap()
+            .wait()
+            .await
+            .unwrap();
+    }
+}
 
 /// Send the request to the real instance
 async fn handle(client_ip: IpAddr, req: Request<Body>) -> Result<Response<Body>, Infallible> {
     // check whether instance exsit
     // Safety: make sure only one instance can start and only after instance start,
     // request can be response
-    let _ = INSTANCE_START
-        .get_or_init(|| async move {
-            let start_time = SystemTime::now();
-            println!(
-                "Init instance at: {}",
-                start_time.duration_since(UNIX_EPOCH).unwrap().as_millis()
-            );
+    unsafe {
+        let _ = INSTANCE
+            .get_or_init(|| async move {
+                let start_time = SystemTime::now();
+                println!(
+                    "Init instance at: {}",
+                    start_time.duration_since(UNIX_EPOCH).unwrap().as_millis()
+                );
 
-            tokio::spawn(async move {
-                Command::new("python3")
-                    .arg("daemon.py")
-                    .spawn()
-                    .expect("fail to boot instance")
+                let child = tokio::spawn(async move {
+                    Command::new("python3")
+                        .arg("daemon.py")
+                        .spawn()
+                        .expect("fail to boot instance")
+                })
+                .await
+                .unwrap();
+
+                // yes, it is a magic number
+                // I don't know how to decide the time to wait for instance start,
+                // but I know `MAX_TRY` works
+                const MAX_TRY: usize = 10000;
+                for i in 0..=MAX_TRY {
+                    if let Ok(_) = hyper_reverse_proxy::call(
+                        client_ip,
+                        INSTANCE_ADDR,
+                        Request::new(Body::empty()),
+                    )
+                    .await
+                    {
+                        break;
+                    }
+                    if i == MAX_TRY {
+                        panic!("Unable to boot instance function")
+                    }
+                }
+
+                child.id().unwrap()
             })
-            .await
-            .unwrap();
-
-            // yes, it is a magic number
-            // I don't know how to decide the time to wait for instance start,
-            // but I know `MAX_TRY` works
-            const MAX_TRY: usize = 10000;
-            for i in 0..=MAX_TRY {
-                if let Ok(_) =
-                    hyper_reverse_proxy::call(client_ip, INSTANCE_ADDR, Request::new(Body::empty()))
-                        .await
-                {
-                    break;
-                }
-                if i == MAX_TRY {
-                    panic!("Unable to boot instance function")
-                }
-            }
-        })
-        .await;
+            .await;
+    }
 
     match hyper_reverse_proxy::call(client_ip, INSTANCE_ADDR, req).await {
         Ok(response) => Ok(response),
